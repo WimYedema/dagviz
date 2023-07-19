d66e9a12b7ef1e9a0f67e9c9eb083d106e9b5fe4
Wim <wim.yedema@gmail.com>
2023-07-19 19:51:44 +0000

    Fix mypy error

--- dagviz/__init__.py
+++ dagviz/__init__.py
@@ -4,7 +4,7 @@ DAGVIZ provides a simple visualization of Directed Acyclic Graphs.
 try:
     import importlib.metadata as importlib_metadata  # type: ignore
 except ModuleNotFoundError:
-    import importlib_metadata  # type: ignore
+    import importlib_metadata
 
 __version__ = importlib_metadata.version(__name__)
 
